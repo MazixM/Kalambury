3a023d056cc262787cb381e28045e10b42b10845
Marcin Milczarek <50635945+MazixM@users.noreply.github.com>
2020-11-24 00:33:04 +0100

    Add time and nick

--- app.js
+++ app.js
@@ -8,10 +8,20 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(jsonString){
+    var message = JSON.parse(jsonString);
+    if(message.msg.length > 3)
+    {
+      io.emit('chat message', getCurrentTime() + " " + message.nick + " - " + message.msg);
+    }
   });
 });
+
+function getCurrentTime()
+{
+  var d = new Date();
+  return d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
